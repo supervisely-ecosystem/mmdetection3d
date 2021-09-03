1942653af8964168b1adb23bd478a4db91c964c7
kashkarov <kashkarov@dedicorp.ru>
2021-09-03 15:45:16 +0300

    debug gpu usage

--- supervisely/serve/src/sly_globals.py
+++ supervisely/serve/src/sly_globals.py
@@ -3,6 +3,7 @@ import supervisely_lib as sly
 import pathlib
 import sys
 import json
+import torch
 
 my_app = sly.AppService()
 api = my_app.public_api
@@ -19,7 +20,13 @@ with open(f"/sessions/{task_id}/repo/supervisely/serve/config.json") as f:
     pretrained_models_cfg = json.load(f)['modal_template_state']['models']
 
 
-device = 'cuda'
+device = ['modal.state.device']
+os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
+iscuda = torch.cuda.is_available()
+sly.logger.debug(f"Is cuda available: {iscuda}")
+if not iscuda:
+    raise RuntimeError("Cuda not available")
+
 
 root_source_path = str(pathlib.Path(sys.argv[0]).parents[3])
 sly.logger.info(f"Root source directory: {root_source_path}")
