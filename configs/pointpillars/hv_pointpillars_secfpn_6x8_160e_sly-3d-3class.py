90a31250a95330c9be3b985e0270e9092ef24d0c
kashkarov <kashkarov@dedicorp.ru>
2021-09-09 15:11:47 +0300

    fix config

--- configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_sly-3d-3class.py
+++ configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_sly-3d-3class.py
@@ -7,12 +7,13 @@ _base_ = [
 point_cloud_range = [0, -39.68, -3, 69.12, 39.68, 1]
 # dataset settings
 data_root = '/data/slyproject'
+dataset_type = 'SuperviselyDataset'
 class_names = ['Pedestrian', 'Cyclist', 'Car']
 # PointPillars adopted a different sampling strategies among classes
 
 # PointPillars uses different augmentation hyper parameters
 train_pipeline = [
-    dict(type='LoadPointsFromFile', coord_type = 'LIDAR', load_dim = 4, use_dim = 4),
+    dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=4, use_dim=4),
     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
     dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
     dict(type='PointsRangeFilter', point_cloud_range=point_cloud_range),
@@ -22,14 +23,59 @@ train_pipeline = [
     dict(type='Collect3D', keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
 ]
 test_pipeline = [
-
-    dict(type='LoadPointsFromFile', coord_type = 'LIDAR', load_dim = 4, use_dim = 4),
+    dict(
+        type='LoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=4,
+        use_dim=4),
+    dict(
+        type='MultiScaleFlipAug3D',
+        img_scale=(1333, 800),
+        pts_scale_ratio=1,
+        flip=False,
+        transforms=[
+            dict(
+                type='GlobalRotScaleTrans',
+                rot_range=[0, 0],
+                scale_ratio_range=[1., 1.],
+                translation_std=[0, 0, 0]),
+            dict(type='RandomFlip3D'),
+            dict(
+                type='PointsRangeFilter', point_cloud_range=point_cloud_range),
+            dict(
+                type='DefaultFormatBundle3D',
+                class_names=class_names,
+                with_label=False),
+            dict(type='Collect3D', keys=['points'])
+        ])
 ]
 
 data = dict(
-    train=dict(dataset=dict(pipeline=train_pipeline, classes=class_names)),
-    val=dict(pipeline=test_pipeline, classes=class_names),
-    test=dict(pipeline=test_pipeline, classes=class_names))
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type='RepeatDataset',
+        times=2,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            pipeline=train_pipeline,
+            classes=class_names,
+            test_mode=False,
+            box_type_3d='LiDAR')),
+    val=dict(
+            type=dataset_type,
+            data_root=data_root,
+            pipeline=test_pipeline,
+            classes=class_names,
+            test_mode=False,
+            box_type_3d='LiDAR'),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        pipeline=test_pipeline,
+        classes=class_names,
+        test_mode=True))
 
 # In practice PointPillars also uses a different schedule
 # optimizer
@@ -42,7 +88,8 @@ optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # PointPillars usually need longer schedule than second, we simply double
 # the training schedule. Do remind that since we use RepeatDataset and
 # repeat factor is 2, so we actually train 160 epochs.
-runner = dict(max_epochs=2)
+runner = dict(max_epochs=4)
 
 # Use evaluation interval=2 reduce the number of evaluation timese
-evaluation = dict(interval=2)
+evaluation = dict(interval=1, pipeline=test_pipeline)
+#evaluation = dict(interval=2)
